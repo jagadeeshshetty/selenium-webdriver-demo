577facc1f565f3d763b36814b189a5a05b5b0f59
JC <jagadeesh.cheluvaraj@gmail.com>
2019-12-14 11:20:31 +0530

    testing applitools VRT validation.

--- test/loginTest.js
+++ test/loginTest.js
@@ -1,5 +1,6 @@
 
 const { Builder } = require('selenium-webdriver');
+const { Eyes } = require('@applitools/eyes-selenium');
 require('chromedriver');
 const assert = require('assert');
 
@@ -7,13 +8,18 @@ describe('Verify Login', function () {
     // by default, 2sec will be mocha timeout. so explicitly set to 30sec timeout.
     this.timeout(30000);
     let driver;
+    let eyes;
 
     beforeEach(async function () {
+        eyes = new Eyes();
+        eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
         driver = await new Builder().forBrowser('chrome').build();
-    })
+        driver = await eyes.open(driver, 'the-internet', this.currentTest.fullTitle(), { width: 1024, height: 768 });
+    });
 
     afterEach(async function () {
         await driver.quit();
+        await eyes.abortIfNotClosed();
     })
 
     it('with valid credentials', async function () {
@@ -31,5 +37,7 @@ describe('Verify Login', function () {
         assert(await driver
             .findElement({ css: '.flash.success' })
             .isDisplayed(), 'Success message not displayed');
+        await eyes.checkWindow('Logged in');
+        await eyes.close();
     });
 });
