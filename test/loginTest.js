2995ef80bdce31fd1369c388058f715f68a9ff2f
JC <jagadeesh.cheluvaraj@gmail.com>
2019-12-18 13:12:15 +0530

    added page object support.

--- test/loginTest.js
+++ test/loginTest.js
@@ -2,14 +2,18 @@
 const { Builder } = require('selenium-webdriver');
 require('chromedriver');
 const assert = require('assert');
+const LoginPage = require('../pages/LoginPage');
 
 describe('Verify Login', function () {
     // by default, 2sec will be mocha timeout. so explicitly set to 30sec timeout.
     this.timeout(30000);
     let driver;
+    let login;
 
     beforeEach(async function () {
         driver = await new Builder().forBrowser('chrome').build();
+        login = new LoginPage(driver);
+        await login.load();
     })
 
     afterEach(async function () {
@@ -17,19 +21,12 @@ describe('Verify Login', function () {
     })
 
     it('with valid credentials', async function () {
-        await driver
-            .get('http://the-internet.herokuapp.com/login');
-        await driver
-            .findElement({ id: 'username' })
-            .sendKeys('tomsmith');
-        await driver
-            .findElement({ id: 'password' })
-            .sendKeys('SuperSecretPassword!');
-        await driver
-            .findElement({ css: 'button' })
-            .click();
-        assert(await driver
-            .findElement({ css: '.flash.success' })
-            .isDisplayed(), 'Success message not displayed');
+        await login.authenticate('tomsmith', 'SuperSecretPassword!');
+        assert(await login.successMessagePresent(), 'Success message not displayed');
+    });
+
+    it('with invalid credentials', async function () {
+        await login.authenticate('invalid', 'invalid')
+        assert(await login.failureMessagePresent(), 'Failure message not displayed');
     });
 });
