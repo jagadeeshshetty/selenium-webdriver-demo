6228763aa7553b5fe4537ad2f3f4724be62ae715
Jagadeesh Cheluvaraj Shetty <jagadeesh.cheluvaraj@gmail.com>
2020-08-22 12:12:32 +0530

    added test run with tag support.

--- test/DynamicLoadingTest.js
+++ test/DynamicLoadingTest.js
@@ -3,14 +3,14 @@ require('./spec_helper');
 const assert = require('assert');
 const DynamicLoadingPage = require('../pages/DynamicLoadingPage');
 
-describe('Verify Dynamic Loading', function () {
+describe('Verify Dynamic Loading @dynamicLoad', function () {
     let dynamicLoading;
 
     beforeEach(async function () {
         dynamicLoading = new DynamicLoadingPage(this.driver);
     })
 
-    it('hidden element', async function () {
+    it('hidden element @P2', async function () {
         await dynamicLoading.loadExample('1');
         assert(await dynamicLoading.isFinishTextPresent(), true, 'Finish text not displayed.');
     });
